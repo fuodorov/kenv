e76351a912550b4c98bda10db29276cf31fd8b44
Vyacheslav Fedorov <57550812+fuodorov@users.noreply.github.com>
2019-11-11 16:24:16 +0700

    Update setuop

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='kenv',
     version='0.0.1',
-    packages=find_packages(),
     author='Vyacheslav Fedorov',
     author_email='fuodorov1998@gmail.com',
+    description='Solver of the Kapchinsky-Vladimir envelope equation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/fuodorov/kenv',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
