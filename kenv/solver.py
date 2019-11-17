760e491215a7021dd0e628380f30395fc2bfac2b
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2019-11-17 21:58:57 +0700

    Update

--- kenv/solver.py
+++ kenv/solver.py
@@ -49,14 +49,21 @@ class Simulation:
         self.gamma = interpolate.interp1d(self.parameter[1:], self.gamma, fill_value=(self.gamma[0], self.gamma[-1]), bounds_error=False)
 
         self.particle_x = {}
+        self.particle_angular_x = {}
         self.particle_y = {}
+        self.particle_angular_y = {}
         self.particle_radius = {}
+
         self.particle_larmor_phase = {}
         self.particle_larmor_x = {}
+        self.particle_larmor_angular_x = {}
         self.particle_larmor_y = {}
+        self.particle_larmor_angular_y = {}
 
-        self.envelope_x = []
-        self.envelope_y = []
+        self.envelope_x = self.beam_x = []
+        self.beam_angular_x = []
+        self.envelope_y = self.beam_y = []
+        self.beam_angular_y = []
 
     def track(self, solver = 'odeint', rtol:float = 1e-8):
         '''Tracking!
@@ -158,22 +165,30 @@ class Simulation:
                 dxdz =  - K_x * sigma_x - dgdz * x / (beta * beta * g) - d2gdz2 * sigma_x/(2 * beta * beta * g)
                 dsigma_ydz = y
                 dydz = - K_y * sigma_y - dgdz * y/(beta * beta * g) - d2gdz2 * sigma_y/(2 * beta * beta * g)
-                dphidz = K_s**0.5
+                dphidz = -K_s**0.5
 
                 return [dsigma_xdz, dxdz, dsigma_ydz, dydz, dphidz]
 
             sol = odeint(dXdz, X0, self.parameter, rtol=rtol)
             self.envelope_x = sol[0:,0]
-            self.envelope_y= sol[0:,2]
+            self.beam_angular_x = sol[0:,1]
+            self.envelope_y = sol[0:,2]
+            self.beam_angular_y = sol[0:,3]
 
             for particle in self.particles.values():
                 X0_particle = [particle.x_position, particle.angular_x, particle.y_position, particle.angular_y, particle.phase]
                 sol = odeint(dXdz_particle, X0_particle, self.parameter, rtol=rtol)
                 self.particle_larmor_x[particle.name] =  sol[0:,0]
+                self.particle_larmor_angular_x[particle.name] =  sol[0:,1]
                 self.particle_larmor_y[particle.name] =  sol[0:,2]
+                self.particle_larmor_angular_y[particle.name] =  sol[0:,3]
                 self.particle_larmor_phase[particle.name] =  sol[0:,4]
+
                 self.particle_x[particle.name] = sol[0:,0]*np.cos(sol[0:,4]) - sol[0:,2]*np.sin(sol[0:,4])
                 self.particle_y[particle.name] = sol[0:,0]*np.sin(sol[0:,4]) + sol[0:,2]*np.cos(sol[0:,4])
+                self.particle_angular_x[particle.name] = sol[0:,1]*np.cos(sol[0:,4]) - sol[0:,3]*np.sin(sol[0:,4])
+                self.particle_angular_y[particle.name] = sol[0:,1]*np.sin(sol[0:,4]) + sol[0:,3]*np.cos(sol[0:,4])
+
                 self.particle_radius[particle.name] = (sol[0:,0]**2 + sol[0:,2]**2)**0.5
 
         elif solver == 'solve_ivp':
@@ -271,24 +286,30 @@ class Simulation:
                 dxdz =  - K_x * sigma_x - dgdz * x / (beta * beta * g) - d2gdz2 * sigma_x/(2 * beta * beta * g)
                 dsigma_ydz = y
                 dydz = - K_y * sigma_y - dgdz * y/(beta * beta * g) - d2gdz2 * sigma_y/(2 * beta * beta * g)
-                dphidz = K_s**0.5
+                dphidz = -K_s**0.5
 
                 return [dsigma_xdz, dxdz, dsigma_ydz, dydz, dphidz]
 
             X0 = np.array(X0)
             sol = solve_ivp(dXdz, t_span=[self.parameter[0],self.parameter[-1]], y0=X0, t_eval=self.parameter, rtol=rtol).y
             self.envelope_x = sol[0,:]
+            self.beam_angular_x = sol[1,:]
             self.envelope_y = sol[2,:]
+            self.beam_angular_y = sol[3,:]
 
             for particle in self.particles.values():
                 X0_particle = np.array([particle.x_position, particle.angular_x, particle.y_position, particle.angular_y, particle.phase])
                 sol = solve_ivp(dXdz_particle, t_span=[self.parameter[0],self.parameter[-1]], y0=X0_particle, t_eval=self.parameter, rtol=rtol).y
-                self.particle_larmor_x[particle.name] =  sol[0,:]
+                self.particle_larmor_x[particle.name] = sol[0,:]
+                self.particle_larmor_angular_x[particle.name] =  sol[1,:]
                 self.particle_larmor_y[particle.name] =  sol[2,:]
+                self.particle_larmor_angular_y[particle.name] =  sol[3,:]
                 self.particle_larmor_phase[particle.name] =  sol[4,:]
+
                 self.particle_x[particle.name] = sol[0,:]*np.cos(sol[4,:]) - sol[2,:]*np.sin(sol[4,:])
                 self.particle_y[particle.name] = sol[0,:]*np.sin(sol[4,:]) + sol[2,:]*np.cos(sol[4,:])
-                self.particle_radius[particle.name] = (sol[0,:]**2 + sol[2,:]**2)**0.5
+                self.particle_angular_x[particle.name] = sol[1,:]*np.cos(sol[4,:]) - sol[3,:]*np.sin(sol[4,:])
+                self.particle_angular_y[particle.name] = sol[1,:]*np.sin(sol[4,:]) + sol[3,:]*np.cos(sol[4,:])
 
 
 Sim = Simulation
