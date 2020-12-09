fc945f12804e9787bf7e3fcb420b1abed8ee4cf0
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2020-12-09 13:10:52 +0700

    Up

--- kenv/__init__.py
+++ kenv/__init__.py
@@ -7,8 +7,6 @@ from .solver import *
 
 __version__ = '0.2.3.0'
 __doc__ = """Kapchinscky ENVelope (KENV) -
- solver of the Kapchinsky-Vladimirsky envelope equation
-
- """
+ solver of the Kapchinsky-Vladimirsky envelope equation"""
 
 __all__ = constants.__all__ + beam.__all__ + accelerator.__all__ + solver.__all__
